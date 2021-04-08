ea9db47ab4c799628dc9ebdf3c491deedaa9c0a5
WDavid Calsin <david2c142@gmail.com>
2021-04-08 18:07:38 -0500

    refactor: 💡 update ejs-1

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,21 @@
 import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello world redis');
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.use(expressEjsLayouts);
+
+app.get('/', function (req, res) {
+  res.locals = {
+    title: 'Ejemplo sensillo de EJS-NODEJS',
+    message: 'This is a message',
+  };
+  res.render('layout');
 });
 
 app.listen(port, () => {
