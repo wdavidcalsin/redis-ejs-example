bc8ef91c3ae0cbf9868d443e782219d08bd9ff8d
WDavid Calsin <david2c142@gmail.com>
2021-04-08 22:11:13 -0500

    feat: 🎸 add removeAll

--- src/config.ts
+++ src/config.ts
@@ -1,21 +1,45 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import expressEjsLayouts from 'express-ejs-layouts';
 import path from 'path';
+import bodyParser from 'body-parser';
+import { type } from 'os';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+type IData = {
+  user: string;
+  password: string;
+};
+
+let data: Array<IData> = [];
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(expressEjsLayouts);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
   res.locals = {
-    title: 'Ejemplo sensillo de EJS-NODEJS',
+    title: 'Simple example of REDIS-EJS-NODEJS',
+    data: data,
     message: 'This is a message',
   };
   res.render('layout');
+});
+
+app.post('/save', (req: Request, res: Response) => {
+  console.log('Se pidio una peticion', req.body.user, req.body.password);
+  data = [...data, { user: req.body.user, password: req.body.password }];
+  res.redirect('/');
+});
+
+app.get('/removeAll', (req: Request, res: Response) => {
+  data = [];
+  res.redirect('/');
 });
 
 app.listen(port, () => {
